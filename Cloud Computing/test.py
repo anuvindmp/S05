fd45aeced7e56f8aca91ac66457be5d017a5967d
Anuvind MP <anuvindmp292003@gmail.com>
2024-12-22 22:56:18 +0530

    more stuff

--- Cloud Computing/test.py
+++ Cloud Computing/test.py
@@ -2,6 +2,6 @@ import requests
 import pprint
 
 url = 'https://llm.biscuitbobby.eu.org/query'
-payload = {"message": "take a picture and tell me what is in front of it via discord", "max_turns": 2}
+payload = {"message": "search for ma meilleur enemie on youtube.", "max_turns": 2}
 response = requests.post(url, json=payload)
 pprint.pprint(response.json()['result'])
